13eb9bd85702b4f7c30e2a6976f932bb8dd0f175
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-07-28 23:36:56 +0300

    added push method to the linked list

--- DataStructures/Class/SinglyLinkedList.js
+++ DataStructures/Class/SinglyLinkedList.js
@@ -25,10 +25,35 @@ class Node {
     }
 }
 
-var first = new Node("Hi");
-first.next = new Node("there");
-first.next.next = new Node("how");
-first.next.next.next = new Node("are");
-first.next.next.next.next = new Node("you");
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
 
-console.log(first);
+class SlinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        var newNode = new Node(val);
+        // if the list is empty
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+var list = new SlinglyLinkedList();
+list.push("Hello");
+list.push("Goodbye");
+list.push("Welcome");
+console.log(list.head.next.next);
