8cbd983f8bf038e7232bfb30d7e796149c218b94
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-12-04 21:37:17 +0200

    added Binary search

--- Algorithms/Searching/BinarySearch.js
+++ Algorithms/Searching/BinarySearch.js
@@ -1,9 +1,22 @@
-function binarySearch(arr, val) {
+function binarySearch(arr, elem) {
     let start = 0;
     let end = arr.length - 1;
     let middle = Math.floor((start + end) / 2);
 
-    while (arr[middle] !== val && start < end) {
-
-    }
+    while (arr[middle] !== elem && start < end) {
+        if (elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    } 
+    if (arr[middle] === elem) {
+        return middle;
+    } 
+    return -1;
 }
+
+console.log(binarySearch([1, 3, 5, 8, 9], 8));
+
+// Time complexity: O(log n)
