e97fafe2e8fa95ac50953528228b7922b081ec37
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-12-17 21:00:53 +0200

    LeetCode: ContainsDuplicate solutin 2

--- JavaScript/Practice/LeetCode/ContainsDuplicate.js
+++ JavaScript/Practice/LeetCode/ContainsDuplicate.js
@@ -4,7 +4,33 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// Solution 1
+// var containsDuplicate = function(nums) {
+//     const set = new Set(nums);
+//     return set.size !== nums.length;
+// };
+
+// Solution 2
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var containsDuplicate = function(nums) {
-    const set = new Set(nums);
-    return set.size !== nums.length;
+    const numsFrequency = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (numsFrequency[nums[i]] === undefined) {
+            numsFrequency[nums[i]] = 1;
+        } else {
+            numsFrequency[nums[i]] += 1;
+        }
+    }
+
+    for (let num in numsFrequency) {
+        if (numsFrequency[num] !== 1) {
+            return true;
+        } 
+    }
+
+    return false;
 };
