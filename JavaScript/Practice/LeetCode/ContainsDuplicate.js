9ab8bd39d3e1e68016a46dc99ccae25167b7fa37
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-12-17 20:57:23 +0200

    LeetCode: ContainsDuplicate

---
+++ JavaScript/Practice/LeetCode/ContainsDuplicate.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set(nums);
+    return set.size !== nums.length;
+};
