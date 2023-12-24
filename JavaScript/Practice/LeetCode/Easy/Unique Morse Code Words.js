e2eb835aa6ec44475665533f2fa0e083cdef4340
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-12-24 13:09:10 +0200

    LeetCode

--- JavaScript/Practice/LeetCode/Easy/Unique Morse Code Words.js
+++ JavaScript/Practice/LeetCode/Easy/Unique Morse Code Words.js
@@ -6,21 +6,14 @@
  */
 var uniqueMorseRepresentations = function(words) {
     const letters = {'a':".-", 'b':"-...", 'c':"-.-.", 'd':"-..", 'e':".", 'f':"..-.", 'g':"--.", 'h':"....", 'i':"..", 'j':".---", 'k':"-.-", 'l':".-..", 'm':"--", 'n':"-.", 'o':"---", 'p':".--.", 'q':"--.-", 'r':".-.", 's':"...", 't':"-", 'u':"..-", 'v':"...-", 'w':".--", 'x':"-..-", 'y':"-.--", 'z':"--.."}
-    // --. .. -.
-    // --.. . -.
     const uniqueCodes = new Set();
     for (let i = 0; i < words.length; i++) {
         let currentCode = "";
         for (let j = 0; j < words[i].length; j++) {
             currentCode += (letters[words[i][j]]);
-            console.log("current code: ", currentCode)
-            console.log("letter: ", letters[words[i][j]]);
 
         }
-        console.log(currentCode)
         uniqueCodes.add(currentCode);
     }
     return uniqueCodes.size;
 };
-
-console.log(uniqueMorseRepresentations(["gin","zen","gig","msg"]));
