49f1d5f038dc0f96e9932971eafb27823af4d1ab
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-01 20:00:28 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Easy/Move Zeroes.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/move-zeroes/description/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let first = 0;
+    let second = 1;
+    while (second < nums.length) {
+        if (nums[first] !== 0) {
+            first++;
+            second++;
+        } else {
+            if (nums[second] !== 0) {
+                let swap = nums[first];
+                nums[first] = nums[second];
+                nums[second] = swap;
+                first++;
+            }
+                second++;
+        }
+    }
+};
