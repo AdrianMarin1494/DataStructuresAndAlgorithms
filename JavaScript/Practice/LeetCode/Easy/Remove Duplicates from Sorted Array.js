579ec6fb559c7791c5c9167f634a971abafb1c06
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-01 17:01:03 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Easy/Remove Duplicates from Sorted Array.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
