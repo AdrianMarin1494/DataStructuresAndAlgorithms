d35387b778eb09e30cefd29b818423cd48299993
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-02 17:44:10 +0200

    graph

--- JavaScript/DataStructures/Graph/Graph.js
+++ JavaScript/DataStructures/Graph/Graph.js
@@ -53,3 +53,15 @@
 //     E: ["D", "F"],
 //     F: ["E", "A"]
 // }
+
+// DIFFERENCES & BIG O
+// |V| - number of vertices
+// |E| - number of edges
+
+// OPERATION	    ADJACENCY LIST	    ADJACENCY MATRIX
+// Add Vertex	    O(1)	            ​O(|V^2|)
+// Add Edge	    O(1)	            O(1)
+// Remove Vertex	O(|V| + |E|)	    ​O(|V^2|)
+// Remove Edge	    O(|E|)	            O(1)
+// Query	        O(|V| + |E|)	    O(1)
+// Storage	        O(|V| + |E|)	    ​O(|V^2|)
