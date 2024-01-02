07b1d5def95d8fb61b258685f34c996cc42de12f
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-02 15:27:26 +0200

    leetcode

---
+++ C#/Practice/LeetCode/Easy/Find the Maximum Achievable Number.cs
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+public class Solution {
+    public int TheMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
