69c26b410026febbc68312ea83e30a88f6ac0308
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-02 15:20:02 +0200

    graph

---
+++ JavaScript/DataStructures/Graph/Graph.js
@@ -0,0 +1,22 @@
+// WHAT ARE GRAPHS
+// A graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph
+// Graph: Nodes + Connections
+
+// USES FOR GRAPHS
+// Social Networks
+// Location / Mapping
+// Routing Algorithms
+// Visual Hierarchy
+// File System Optimizations
+
+// ESSENTIAL GRAPH TERMS
+// Vertex - a node
+// Edge - connection between nodes
+// Weighted/Unweighted - values assigned to distances between vertices
+// Directed/Undirected - directions assigned to distanced between vertices
+
+// TYPES OF GRAPHS
+// UNDIRECTED GRAPH
+// DIRECTED GRAPH
+// WEIGHTED GRAPH
+// UNWEIGHTED GRAPH
