0bc2d678579f00f8e674ab56524844bd350e3ae8
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-02 15:29:59 +0200

    leetcode

---
+++ Python/Practice/LeetCode/Easy/Find the Maximum Achievable Number.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2 * t
+    
