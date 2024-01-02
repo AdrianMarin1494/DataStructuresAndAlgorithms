3a0f2105d42df15fdafcad569b7c02b4ec7d4414
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-02 15:47:01 +0200

    graph

--- JavaScript/DataStructures/Graph/Graph.js
+++ JavaScript/DataStructures/Graph/Graph.js
@@ -20,3 +20,26 @@
 // DIRECTED GRAPH
 // WEIGHTED GRAPH
 // UNWEIGHTED GRAPH
+
+// HOW DO WE STORE THIS???
+// ADJACENCY MATRIX
+// ADJACENCY LIST
+
+// ADJACENCY MATRIX
+// -	A	B	C	D	E	F
+// A	0	1	0	0	0	1
+// B	1	0	1	0	0	0
+// C	0	1	0	1	0	0
+// D	0	0	1	0	1	0
+// E	0	0	0	1	0	1
+// F	1	0	0	0	1	0
+
+// ADJACENCY LIST
+//    [
+// 0    [1,5],
+// 1    [0,2],
+// 2    [1,3],
+// 3    [2,4],
+// 4    [3,5],
+// 5    [4,0]
+//    ]
