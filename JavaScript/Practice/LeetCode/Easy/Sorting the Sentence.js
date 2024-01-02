b3f19c627b9ccfbd5d87f9d73bbda2747c1e73bb
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-02 21:09:01 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Easy/Sorting the Sentence.js
+++ JavaScript/Practice/LeetCode/Easy/Sorting the Sentence.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/sorting-the-sentence/description/
+
 /**
  * @param {string} s
  * @return {string}
@@ -6,13 +8,9 @@ var sortSentence = function(s) {
     const sArray = s.split(" ");
     sArray.sort((a, b) => a[a.length - 1] - b[b.length - 1]);
     let result = sArray[0].slice(0, sArray[0].length - 1);
-    // console.log(sArray);
     for (let i = 1; i < sArray.length; i++) {
         let word = sArray[i].slice(0, sArray[i].length - 1);
-        // console.log(sArray[i].slice(0, sArray[i].length - 1))
         result = result.concat(" " + word);
     }
-    console.log(result);
+    return result;
 };
-
-sortSentence("is2 sentence4 This1 a3");
