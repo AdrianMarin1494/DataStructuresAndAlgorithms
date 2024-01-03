ed72a3540d28fe91627fd7b2339f0d91b8aecf35
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-03 21:48:47 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Easy/Faulty Keyboard.js
+++ JavaScript/Practice/LeetCode/Easy/Faulty Keyboard.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/faulty-keyboard/description/
+
 /**
  * @param {string} s
  * @return {string}
@@ -6,15 +8,10 @@ var finalString = function(s) {
     let faultyString = "";
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "i") {
-            console.log("before: ", faultyString)
             faultyString = faultyString.split("").reverse().join("");
-            console.log("reverse: ", faultyString)
         } else {
             faultyString = faultyString.concat(s[i]);
         }
     }
-    console.log(faultyString);
     return faultyString;
 };
-
-finalString("string");
