b3971b4269d204459beda744c65de60d4ceeefae
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-04 20:12:01 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Easy/Climbing Stairs.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/climbing-stairs/description/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let arr = [1, 1];
+    for(let i=2; i<=n; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+    return arr[n];
+};
