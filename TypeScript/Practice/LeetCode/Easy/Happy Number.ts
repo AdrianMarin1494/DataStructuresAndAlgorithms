9a1562e2287627c7daebcb1db3020bd961096da7
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-04 21:16:07 +0200

    leetcode

---
+++ TypeScript/Practice/LeetCode/Easy/Happy Number.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/happy-number/description/
+
+function isHappy(n: number): boolean {
+    function isHappyRecursion(newN) {
+        let stringNumber = new String(newN);
+        if (stringNumber.length === 1) {
+            if (newN === 1 || newN === 7) {
+                return true;
+            }
+            if (newN !== 1) {
+                console.log(false);
+                return false;
+            }
+        }
+
+        let sum = 0;
+
+        for (let i = 0; i < stringNumber.length; i++) {
+            sum += Number(stringNumber[i])**2;
+        }
+        return isHappyRecursion(sum);
+    }
+    return isHappyRecursion(n);  
+};
