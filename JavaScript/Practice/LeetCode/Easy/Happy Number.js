147287c7b04f389a8182ddf5e227e29e840958c0
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-04 21:15:11 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Easy/Happy Number.js
+++ JavaScript/Practice/LeetCode/Easy/Happy Number.js
@@ -7,10 +7,8 @@
 var isHappy = function(n) {
     function isHappyRecursion(newN) {
         let stringNumber = new String(newN);
-        console.log("n: ", newN)
         if (stringNumber.length === 1) {
             if (newN === 1 || newN === 7) {
-                console.log("true")
                 return true;
             }
             if (newN !== 1) {
@@ -24,11 +22,7 @@ var isHappy = function(n) {
         for (let i = 0; i < stringNumber.length; i++) {
             sum += Number(stringNumber[i])**2;
         }
-
-        console.log("sum: ", sum);
         return isHappyRecursion(sum);
     }
     return isHappyRecursion(n);
 };
-
-isHappy(19);
