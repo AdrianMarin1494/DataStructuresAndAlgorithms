10243a89ac1161bcda62c5aad7fe3df9f15688fe
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-04 21:56:58 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Contains Duplicate II.ts
+++ TypeScript/Practice/LeetCode/Easy/Contains Duplicate II.ts
@@ -1,2 +1,17 @@
 // https://leetcode.com/problems/contains-duplicate-ii/description/
 
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const numsAndIndexes = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (numsAndIndexes[nums[i]] === undefined) {
+            numsAndIndexes[nums[i]] = i;
+        } else {
+            if (Math.abs(numsAndIndexes[nums[i]] - i) <= k) {
+                return true;
+            }
+            numsAndIndexes[nums[i]] = i;
+        }
+    }
+    return false;  
+};
