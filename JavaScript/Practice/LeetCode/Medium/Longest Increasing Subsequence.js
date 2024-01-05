e9ba49d7eb50a968acbf3fa8a13dded2d03ccba3
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-05 17:57:41 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Medium/Longest Increasing Subsequence.js
+++ JavaScript/Practice/LeetCode/Medium/Longest Increasing Subsequence.js
@@ -1,2 +1,21 @@
 // https://leetcode.com/problems/longest-increasing-subsequence/description/?envType=daily-question&envId=2024-01-05
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let values = [];
+    let result = -1;
+    for(let i = 0; i < nums.length; i++) {
+        values[i] = 1;
+        for(let j = 0; j < i; j++) {
+            if(nums[i] > nums[j]){
+                values[i] = Math.max(values[i], values[j]+1);
+            }
+        }
+        result = Math.max(result, values[i]);
+    }
+    console.log(values);
+    return result;
+};
