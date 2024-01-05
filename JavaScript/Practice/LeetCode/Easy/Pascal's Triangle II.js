de7931ba67bfe692c8ced849d230c6fd54e8841b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-05 21:11:22 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Easy/Pascal's Triangle II.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/pascals-triangle-ii/description/
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    const triangle = [[1], [1, 1]];
+
+    for (let i = 2; i < rowIndex + 1; i++) {
+        const row = Array(i + 1).fill(1);
+
+        for (let j = 1; j < row.length - 1; j++) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+
+        triangle.push(row);
+    }
+
+    return triangle[rowIndex];
+};
