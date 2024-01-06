8237c53faa4114f07ed4bb6294260602130a255b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-06 15:37:27 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Minimum Number Game.cs
+++ C#/Practice/LeetCode/Easy/Minimum Number Game.cs
@@ -2,19 +2,19 @@
 
 public class Solution {
     public int[] NumberGame(int[] nums) {
-    Array.Sort(nums);
-    Array.Reverse(nums);
-    List<int> resList = new List<int>();
-    int first;
-    int second;
-    while (nums.Length > 0) {
-        first = nums[nums.Length - 1];
-        nums = nums[..^1];
-        second = nums[nums.Length - 1];
-        nums = nums[..^1];
-        resList.Add(second);
-        resList.Add(first);
-    }
-    return resList.ToArray();      
+        Array.Sort(nums);
+        Array.Reverse(nums);
+        List<int> resList = new List<int>();
+        int first;
+        int second;
+        while (nums.Length > 0) {
+            first = nums[nums.Length - 1];
+            nums = nums[..^1];
+            second = nums[nums.Length - 1];
+            nums = nums[..^1];
+            resList.Add(second);
+            resList.Add(first);
+        }
+        return resList.ToArray();      
     }
 }
