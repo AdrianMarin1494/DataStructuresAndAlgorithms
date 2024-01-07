2be437ad5e72638512ed703358427c261839a8f7
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-07 12:34:21 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Maximum Sum With Exactly K Elements.cs
+++ C#/Practice/LeetCode/Easy/Maximum Sum With Exactly K Elements.cs
@@ -2,14 +2,14 @@
 
 public class Solution {
     public int MaximizeSum(int[] nums, int k) {
-    Array.Sort(nums);
+        Array.Sort(nums);
 
-    int kSum = 0;
+        int kSum = 0;
 
-    for (int i = 0; i < k; i++) {
-        kSum += i;
-    }
-    
-    return nums[nums.Length - 1] * k + kSum;  
+        for (int i = 0; i < k; i++) {
+            kSum += i;
+        }
+        
+        return nums[nums.Length - 1] * k + kSum;  
     }
 }
