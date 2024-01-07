f72ad8dcde831a11f0ea039b870be3050555b015
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-07 11:30:11 +0200

    leetcode

---
+++ JavaScript/Testing.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    // create an string from the number, create an array from the string
+    // iterate over the array
+    // check if one value is 6 an change to 9, after break from the loop
+    // create a var to store the number version of the array, return it
+
+    const digits = [...num.toString()];
+    console.log("digits: ", digits)
+
+    for (let i = 0; i < digits.length; i++) {
+        console.log("digits[i]: ", digits[i])
+        if (digits[i] === "6") {
+            console.log("on if: ", digits[i])
+            digits[i] = "9";
+            break;
+        }
+    }
+
+    let result = Number(digits.join(""));
+    console.log(result);
+    return result;
+};
+
+maximum69Number(9669);
