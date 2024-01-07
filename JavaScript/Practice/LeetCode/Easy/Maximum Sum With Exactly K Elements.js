0d8539d53db88583f831470fc42d0205cafb35e5
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-07 12:30:01 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Easy/Maximum Sum With Exactly K Elements.js
+++ JavaScript/Practice/LeetCode/Easy/Maximum Sum With Exactly K Elements.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/description/
+
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -7,14 +9,14 @@ var maximizeSum = function(nums, k) {
     // sort the array
     // select the last item of the array
     // return the selected item or * k + k -1
+
     nums.sort((a, b) => a - b);
-    console.log("nums: ", nums)
+
     let kSum = 0;
+
     for (let i = 0; i < k; i++) {
         kSum += i;
     }
-    console.log("res: ", nums[nums.length - 1] * k + kSum)
+    
     return nums[nums.length - 1] * k + kSum;
 };
-
-maximizeSum([1,2,3,4,5], 3);
