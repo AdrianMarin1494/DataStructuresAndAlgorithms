bf90ff61d96a5c60693fbf17a68349e6b5ac5ea8
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-08 20:24:42 +0200

    leetcode

---
+++ TypeScript/Practice/LeetCode/Easy/Check if a String Is an Acronym of Words.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/description/
+
+function isAcronym(words: string[], s: string): boolean {
+    if (words.length !== s.length) return false;
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i][0] !== s[i][0]) {
+            return false;
+        }
+    }
+
+    return true;
+};
