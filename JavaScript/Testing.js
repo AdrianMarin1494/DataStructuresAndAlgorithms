2fa3a649d95df8488ac97bd61fd1c77edd1fdf7f
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-08 20:09:49 +0200

    leetcode

--- JavaScript/Testing.js
+++ JavaScript/Testing.js
@@ -1,20 +1,15 @@
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
+ * @param {string} s
+ * @return {string}
  */
-var maximizeSum = function(nums, k) {
-    // sort the array
-    // select the last item of the array
-    // return the selected item or * k + k -1
-    nums.sort((a, b) => a - b);
-    console.log("nums: ", nums)
-    let kSum = 0;
-    for (let i = 0; i < k; i++) {
-        kSum += i;
+var reverseWords = function(s) {
+    let words = s.split(" ");
+    console.log("words: ", words)
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].split("").reverse().join("");
+        console.log("words[i]: ", words[i])
     }
-    console.log("res: ", nums[nums.length - 1] * k + kSum)
-    return nums[nums.length - 1] * k + kSum;
+    return words.join(" ");
 };
 
-maximizeSum([1,2,3,4,5], 3);
+reverseWords("Let's take LeetCode contest");
