095c64020f1bd275ea1260a5ee55c8d1e82b327f
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-08 20:28:32 +0200

    leetcode

---
+++ C#/Practice/LeetCode/Easy/Check if a String Is an Acronym of Words.cs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/description/
+
+public class Solution {
+    public bool IsAcronym(IList<string> words, string s) {
+        if (words.Count != s.Length) return false;
+
+        for (int i = 0; i < words.Count; i++) {
+            if (words[i][0] != s[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
