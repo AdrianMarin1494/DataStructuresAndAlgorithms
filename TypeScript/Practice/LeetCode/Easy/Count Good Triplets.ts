23437cb846e63fd7ae35bb9440d6e43a23fca4e7
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-09 14:59:45 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Count Good Triplets.ts
+++ TypeScript/Practice/LeetCode/Easy/Count Good Triplets.ts
@@ -1,2 +1,21 @@
 // https://leetcode.com/problems/count-good-triplets/description/
 
+function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
+    let goodTripletsCounter: number = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            for (let k = j + 1; k < arr.length; k++) {
+                if (
+                    Math.abs(arr[i] - arr[j]) <= a && 
+                    Math.abs(arr[j] - arr[k]) <= b &&
+                    Math.abs(arr[i] - arr[k]) <= c
+                ) {
+                    goodTripletsCounter++;
+                }
+            }
+        }
+    }
+
+    return goodTripletsCounter;
+};
