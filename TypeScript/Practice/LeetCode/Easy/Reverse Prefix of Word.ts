7bc6e6ac377719034f1b01462e5b8032b93ebfd0
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-09 19:33:58 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Reverse Prefix of Word.ts
+++ TypeScript/Practice/LeetCode/Easy/Reverse Prefix of Word.ts
@@ -1,2 +1,17 @@
 // https://leetcode.com/problems/reverse-prefix-of-word/description/
 
+function reversePrefix(word: string, ch: string): string {
+    const wordLetters: string[] = [];
+    let reversed: boolean = false;
+
+    for (let i = 0; i < word.length; i++) {
+        wordLetters.push(word[i]);
+        
+        if (word[i] === ch && !reversed) {
+            wordLetters.reverse();
+            reversed = true;
+        }
+    }
+
+    return wordLetters.join("");    
+};
