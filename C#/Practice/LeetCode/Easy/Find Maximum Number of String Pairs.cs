c23ed29a7471ec4c1ce271b883e3dcfa2e451a78
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-09 17:34:08 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Find Maximum Number of String Pairs.cs
+++ C#/Practice/LeetCode/Easy/Find Maximum Number of String Pairs.cs
@@ -1,2 +1,17 @@
 // https://leetcode.com/problems/find-maximum-number-of-string-pairs/description/
 
+public class Solution {
+    public int MaximumNumberOfStringPairs(string[] words) {
+        int pairsCounter = 0;
+
+        for (int i = 0; i < words.Length; i++) {
+            string reversedWord = new string(words[i].ToCharArray().Reverse().ToArray());
+            int idx = Array.IndexOf(words, reversedWord);
+            if (idx > - 1 && idx != i) {
+                pairsCounter++;
+            }
+        }
+        
+        return pairsCounter / 2; 
+    }
+}
