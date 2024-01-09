791b40724d4e9cc5c1396aa9a1507399d8e0a1af
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-09 17:07:56 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Find Maximum Number of String Pairs.ts
+++ TypeScript/Practice/LeetCode/Easy/Find Maximum Number of String Pairs.ts
@@ -1,2 +1,14 @@
 // https://leetcode.com/problems/find-maximum-number-of-string-pairs/description/
 
+function maximumNumberOfStringPairs(words: string[]): number {
+    let pairsCounter: number = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        let reversedWord = words[i].split("").reverse().join("");
+        if (words.indexOf(reversedWord) > - 1 && words.indexOf(reversedWord) !== i) {
+            pairsCounter++;
+        }
+    }
+    
+    return Math.floor(pairsCounter / 2);  
+};
