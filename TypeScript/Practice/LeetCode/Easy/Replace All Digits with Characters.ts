863326f5305d498f5586313c2c5c497fede61544
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-09 15:48:31 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Replace All Digits with Characters.ts
+++ TypeScript/Practice/LeetCode/Easy/Replace All Digits with Characters.ts
@@ -1,2 +1,46 @@
 // https://leetcode.com/problems/replace-all-digits-with-characters/
 
+function replaceDigits(s: string): string {
+    const letterValues = {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+        e: 4,
+        f: 5,
+        g: 6,
+        h: 7,
+        i: 8,
+        j: 9,
+        k: 10,
+        l: 11,
+        m: 12,
+        n: 13,
+        o: 14,
+        p: 15,
+        q: 16,
+        r: 17,
+        s: 18,
+        t: 19,
+        u: 20,
+        v: 21,
+        w: 22,
+        x: 23,
+        y: 24,
+        z: 25
+    }
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    function shift(character, digit) {
+        let letterIndex = Number(letterValues[character]) + Number(digit);
+        return letters[letterIndex];
+    }
+
+    const sCharacters = s.split("");
+    
+    for (let i = 1; i < sCharacters.length; i += 2) {
+        sCharacters[i] = shift(sCharacters[i-1], sCharacters[i]);
+    }
+
+    return sCharacters.join(""); 
+};
