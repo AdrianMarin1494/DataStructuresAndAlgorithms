1441d848366dc12440c9ee0b9911c941be6bb537
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 20:35:42 +0200

    leetcode

---
+++ C#/Practice/LeetCode/Easy/Find the Difference of Two Arrays.cs
@@ -0,0 +1,2 @@
+// https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
---
+++ JavaScript/Practice/LeetCode/Easy/Find the Difference of Two Arrays.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    const nums1Distinct = [];
+    const nums2Distinct = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+        if (!nums2.includes(nums1[i]) && !nums1Distinct.includes(nums1[i])) {
+            nums1Distinct.push(nums1[i]);
+        }
+    }
+    for (let i = 0; i < nums2.length; i++) {
+        if (!nums1.includes(nums2[i]) && !nums2Distinct.includes(nums2[i])) {
+            nums2Distinct.push(nums2[i]);
+        }
+    }
+    return [nums1Distinct, nums2Distinct];
+};
