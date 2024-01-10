0b89b8dd8f156b7df80d38c837961a7e415b74dd
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 21:06:51 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Sum of Digits in Base K.cs
+++ C#/Practice/LeetCode/Easy/Sum of Digits in Base K.cs
@@ -1,2 +1,14 @@
 // https://leetcode.com/problems/sum-of-digits-in-base-k/description/
 
+public class Solution {
+    public int SumBase(int n, int k) {
+        int sum = 0;
+
+        while ( n > 0){
+            sum += n%k;
+            n = ~~(n/k);
+        }
+
+        return sum;       
+    }
+}
