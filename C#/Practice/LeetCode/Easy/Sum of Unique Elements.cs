b035361d3260dc13c9c4e9f786192bb617e65011
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 21:15:24 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Sum of Unique Elements.cs
+++ C#/Practice/LeetCode/Easy/Sum of Unique Elements.cs
@@ -1,2 +1,15 @@
 // https://leetcode.com/problems/sum-of-unique-elements/description/
 
+public class Solution {
+    public int SumOfUnique(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (Array.IndexOf(nums, nums[i]) == Array.LastIndexOf(nums, nums[i])) {
+                sum += nums[i];
+            }
+        }
+
+        return sum;        
+    }
+}
