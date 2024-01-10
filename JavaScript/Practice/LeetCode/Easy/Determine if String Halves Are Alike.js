54430100893081857f41b3b2c3ee3362b5eeab8c
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 20:57:38 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Easy/Determine if String Halves Are Alike.js
+++ JavaScript/Practice/LeetCode/Easy/Determine if String Halves Are Alike.js
@@ -12,12 +12,15 @@ var halvesAreAlike = function(s) {
     for (let i = 0; i < s.length / 2; i++) {
         let firstHalfLetter = s[i];
         let secondHalfLetter = s[s.length - i - 1];
+
         if (vowels.includes(firstHalfLetter)) {
             firstHalf.push(firstHalfLetter);
         }
+        
         if (vowels.includes(secondHalfLetter)) {
             secondHalf.push(secondHalfLetter);
         }
     }
-return firstHalf.length === secondHalf.length;    
+
+    return firstHalf.length === secondHalf.length;    
 };
