4ac0601bb305e2c9aeb4bfa5600d3cbf794add1b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 21:05:34 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Sum of Digits in Base K.ts
+++ TypeScript/Practice/LeetCode/Easy/Sum of Digits in Base K.ts
@@ -1,2 +1,12 @@
 // https://leetcode.com/problems/sum-of-digits-in-base-k/description/
 
+function sumBase(n: number, k: number): number {
+    let sum: number = 0
+
+    while (n > 0){
+        sum += n%k
+        n = ~~(n/k)
+    }
+    
+    return sum      
+};
