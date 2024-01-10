63d986758a90d7aa74c689343690b7547e621af5
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 21:00:10 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Determine if String Halves Are Alike.cs
+++ C#/Practice/LeetCode/Easy/Determine if String Halves Are Alike.cs
@@ -1,2 +1,28 @@
 // https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
 
+public class Solution {
+    public bool HalvesAreAlike(string s) {
+        List<char> firstHalf = new List<char>();
+        List<char> secondHalf = new List<char>();
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            char firstHalfLetter = s[i];
+            char secondHalfLetter = s[s.Length - i - 1];
+
+            if (Array.Exists(vowels, v => v == firstHalfLetter))
+            {
+                firstHalf.Add(firstHalfLetter);
+            }
+
+            if (Array.Exists(vowels, v => v == secondHalfLetter))
+            {
+                secondHalf.Add(secondHalfLetter);
+            }
+        }
+
+        return firstHalf.Count == secondHalf.Count;
+    }
+
+}
