a4b111e431fe442bbd0c9ad82f9901de281c3167
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 21:13:34 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Sum of Unique Elements.ts
+++ TypeScript/Practice/LeetCode/Easy/Sum of Unique Elements.ts
@@ -1,2 +1,13 @@
 // https://leetcode.com/problems/sum-of-unique-elements/description/
 
+function sumOfUnique(nums: number[]): number {
+    let sum: number = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i])) {
+            sum += nums[i];
+        }
+    }
+
+    return sum;    
+};
