5ee67831e83640a3aed23cb331e6f54132298ac1
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-10 20:58:27 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Determine if String Halves Are Alike.ts
+++ TypeScript/Practice/LeetCode/Easy/Determine if String Halves Are Alike.ts
@@ -1,2 +1,22 @@
 // https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
 
+function halvesAreAlike(s: string): boolean {
+    const firstHalf: string[] = [];
+    const secondHalf: string[] = [];
+    const vowels: string[] = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+    for (let i = 0; i < s.length / 2; i++) {
+        let firstHalfLetter = s[i];
+        let secondHalfLetter = s[s.length - i - 1];
+
+        if (vowels.includes(firstHalfLetter)) {
+            firstHalf.push(firstHalfLetter);
+        }
+        
+        if (vowels.includes(secondHalfLetter)) {
+            secondHalf.push(secondHalfLetter);
+        }
+    }
+
+    return firstHalf.length === secondHalf.length;      
+};
