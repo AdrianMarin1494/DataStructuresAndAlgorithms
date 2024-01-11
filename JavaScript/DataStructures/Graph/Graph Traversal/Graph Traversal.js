112dc9dd8303cfc888f224ab774461aa2de88395
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-11 09:07:31 +0200

    leetcode

---
+++ JavaScript/DataStructures/Graph/Graph Traversal/Graph Traversal.js
@@ -0,0 +1,10 @@
+// Visiting/Updating/Checking each vertex in a graph
+
+// GRAPH TRAVERSAL USES
+// Peer to peer networking
+// Web crawlers
+// Finding "closest" matches/recommendations
+// Shortest path problems
+// GPS Navigation
+// Solving mazes
+// AI (shortest path to win the game)
