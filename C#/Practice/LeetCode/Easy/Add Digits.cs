7173b2e758e75c704fe2750e60b15d74862dcd81
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-12 14:15:54 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Add Digits.cs
+++ C#/Practice/LeetCode/Easy/Add Digits.cs
@@ -1,2 +1,21 @@
 // https://leetcode.com/problems/add-digits/description/
 
+public class Solution {
+    public int AddDigits(int num) {
+        string numStr = num.ToString();
+        char[] digits = numStr.ToCharArray();
+        int sum = 0;
+
+        while (digits.Length > 1) {
+            for (int i = 0; i < digits.Length; i++) {
+                sum += int.Parse(digits[i].ToString());
+            }
+
+            numStr = sum.ToString();
+            digits = numStr.ToCharArray();
+            sum = 0;
+        }
+
+        return int.Parse(digits[0].ToString());
+    }
+}
