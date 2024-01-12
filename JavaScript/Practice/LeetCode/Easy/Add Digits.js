9963874947d21385561e07dd7111676ebe2c3e3d
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-12 14:09:19 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Easy/Add Digits.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/add-digits/description/
+
+// https://leetcode.com/problems/add-digits/
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    let digits = new String(num).split("");
+    let sum = 0;
+
+    while (digits.length > 1) {
+        for (let i = 0; i < digits.length; i++) {
+            sum += Number(digits[i])
+        }
+
+        digits = new String(sum).split("");
+        sum = 0;
+    }
+
+    return Number(digits[0]);
+};
