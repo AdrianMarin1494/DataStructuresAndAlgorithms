f3e5af6bf8c2f0d14a0e2f16a9bcb021ebcebc01
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-12 14:10:28 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Add Digits.ts
+++ TypeScript/Practice/LeetCode/Easy/Add Digits.ts
@@ -1,2 +1,17 @@
 // https://leetcode.com/problems/add-digits/description/
 
+function addDigits(num: number): number {
+    let digits: string[] = new String(num).split("");
+    let sum: number = 0;
+
+    while (digits.length > 1) {
+        for (let i = 0; i < digits.length; i++) {
+            sum += Number(digits[i])
+        }
+
+        digits = new String(sum).split("");
+        sum = 0;
+    }
+
+    return Number(digits[0]);
+};
