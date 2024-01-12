ffb0c887f5492cb6e22ea5b7fbf96c51d8207f2b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-12 14:33:09 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Add Strings.cs
+++ C#/Practice/LeetCode/Easy/Add Strings.cs
@@ -1,2 +1,11 @@
 // https://leetcode.com/problems/add-strings/description/
 
+public class Solution {
+    public string AddStrings(string num1, string num2) {
+        BigInteger bigint1 = BigInteger.Parse(num1);
+        BigInteger bigint2 = BigInteger.Parse(num2);
+        BigInteger result = bigint1 + bigint2;
+
+        return result.ToString();
+    }
+}
