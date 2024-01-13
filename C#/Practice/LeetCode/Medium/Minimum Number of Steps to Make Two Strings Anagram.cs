f7b58ff81ef77c3e3c912ac3b51bf615c187e21d
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-13 09:11:15 +0200

    leetcode

--- C#/Practice/LeetCode/Medium/Minimum Number of Steps to Make Two Strings Anagram.cs
+++ C#/Practice/LeetCode/Medium/Minimum Number of Steps to Make Two Strings Anagram.cs
@@ -1,1 +1,46 @@
 // https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/description/?envType=daily-question&envId=2024-01-13
+
+public class Solution {
+    public int MinSteps(string s, string t) {
+        Dictionary<char, int> sLetters = new Dictionary<char, int>();
+        Dictionary<char, int> tLetters = new Dictionary<char, int>();
+        int stepsCounter = 0;
+
+        int minLength = Math.Min(s.Length, t.Length);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            if (!sLetters.ContainsKey(s[i]))
+            {
+                sLetters[s[i]] = 1;
+            }
+            else
+            {
+                sLetters[s[i]] += 1;
+            }
+
+            if (!tLetters.ContainsKey(t[i]))
+            {
+                tLetters[t[i]] = 1;
+            }
+            else
+            {
+                tLetters[t[i]] += 1;
+            }
+        }
+
+        foreach (var key in sLetters.Keys)
+        {
+            if (!tLetters.ContainsKey(key))
+            {
+                stepsCounter += sLetters[key];
+            }
+            else if (sLetters[key] > tLetters[key])
+            {
+                stepsCounter += sLetters[key] - tLetters[key];
+            }
+        }
+
+        return stepsCounter;
+    }
+}
--- JavaScript/Practice/LeetCode/Medium/Minimum Number of Steps to Make Two Strings Anagram.js
+++ JavaScript/Practice/LeetCode/Medium/Minimum Number of Steps to Make Two Strings Anagram.js
@@ -15,18 +15,18 @@ var minSteps = function(s, t) {
     // if the value is greater on s than t, add the difference on the steps counter
     // return the steps counter
 
-// s {b: 2, a: 1}
-// t {b: 1, a: 2}
-//     2 - 1 = 1
+    // s {b: 2, a: 1}
+    // t {b: 1, a: 2}
+    //     2 - 1 = 1
 
-// s  {l: 1, e: 3, t: 1, c: 1, o: 1, d: 1}
-// t: {p: 1, r: 1, a: 1, c: 2, t: 1, i: 1, e: 1}
-//     l + 1
-//     e + 2
-//     t + 0
-//     c + 0
-//     o + 1
-//     d + 1
+    // s  {l: 1, e: 3, t: 1, c: 1, o: 1, d: 1}
+    // t: {p: 1, r: 1, a: 1, c: 2, t: 1, i: 1, e: 1}
+    //     l + 1
+    //     e + 2
+    //     t + 0
+    //     c + 0
+    //     o + 1
+    //     d + 1
 
     const sLetters = {};
     const tLetters = {};
