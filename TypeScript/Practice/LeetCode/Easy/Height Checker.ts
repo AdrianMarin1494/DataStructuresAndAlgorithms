fcc545289210da8696bbc167a8a0b279c63520c3
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-14 15:50:05 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Height Checker.ts
+++ TypeScript/Practice/LeetCode/Easy/Height Checker.ts
@@ -1,2 +1,14 @@
 // https://leetcode.com/problems/height-checker/description/
 
+function heightChecker(heights: number[]): number {
+    const sortedHeights: number[] = [...heights].sort((a, b) => a - b);
+    let notMatching: number = 0;
+    
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== sortedHeights[i]) {
+            notMatching++;
+        }
+    }
+    
+    return notMatching;    
+};
