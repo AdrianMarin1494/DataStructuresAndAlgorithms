db7028e7acbc56a67fddd4d8c9938913ad6c609b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-14 18:20:49 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Check if Word Equals Summation of Two Words.cs
+++ C#/Practice/LeetCode/Easy/Check if Word Equals Summation of Two Words.cs
@@ -1,2 +1,23 @@
 // https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
 
+public class Solution {
+    public bool IsSumEqual(string firstWord, string secondWord, string targetWord) {
+        int firstWordValue = GetNumberFromWord(firstWord);
+        int secondWordValue = GetNumberFromWord(secondWord);
+        int targetWordValue = GetNumberFromWord(targetWord);
+
+        return firstWordValue + secondWordValue == targetWordValue;
+    }
+
+        private int GetNumberFromWord(string word) {
+        string lettersValue = "abcdefghijklmnopqrstuvwxyz";
+        string concatenatedValue = "";
+
+        foreach (char letter in word) {
+            int index = lettersValue.IndexOf(letter);
+            concatenatedValue += index.ToString();
+        }
+
+        return int.Parse(concatenatedValue);
+    }
+}
