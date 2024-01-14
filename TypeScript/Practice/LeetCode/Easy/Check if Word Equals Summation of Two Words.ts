0288c1a9102b30e240c2fec1f968d26776ffa70c
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-14 18:18:46 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Check if Word Equals Summation of Two Words.ts
+++ TypeScript/Practice/LeetCode/Easy/Check if Word Equals Summation of Two Words.ts
@@ -1,2 +1,47 @@
 // https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
 
+function isSumEqual(firstWord: string, secondWord: string, targetWord: string): boolean {
+    const firstWordValue = getNumberFromWord(firstWord);
+    const secondWordValue = getNumberFromWord(secondWord);
+    const targetWordValue = getNumberFromWord(targetWord);
+
+    return firstWordValue + secondWordValue === targetWordValue;
+
+    function getNumberFromWord(word) {
+        const lettersValue = {
+            a: "0",
+            b: "1",
+            c: "2",
+            d: "3",
+            e: "4",
+            f: "5",
+            g: "6",
+            h: "7",
+            i: "8",
+            j: "9",
+            k: "10",
+            l: "11",
+            m: "12",
+            n: "13",
+            o: "14",
+            p: "15",
+            q: "16",
+            r: "17",
+            s: "18",
+            t: "19",
+            u: "20",
+            v: "21",
+            w: "22",
+            x: "23",
+            y: "24",
+            z: "25"
+        };
+        let concatenatedValue = "";
+        
+        for (let i = 0; i < word.length; i++) {
+            concatenatedValue += lettersValue[word[i]];
+        }
+        
+        return Number(concatenatedValue);
+    }   
+};
