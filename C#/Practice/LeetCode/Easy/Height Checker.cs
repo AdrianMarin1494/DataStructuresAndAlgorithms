54872c83b7f32e0c760cf99addf8fe65fcf803c9
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-14 15:55:07 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Height Checker.cs
+++ C#/Practice/LeetCode/Easy/Height Checker.cs
@@ -1,2 +1,17 @@
 // https://leetcode.com/problems/height-checker/description/
 
+public class Solution {
+    public int HeightChecker(int[] heights) {
+        int[] sortedHeights = heights.ToArray();
+        Array.Sort(sortedHeights);
+        int notMatching = 0;
+        
+        for (int i = 0; i < heights.Length; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                notMatching++;
+            }
+        }
+        
+        return notMatching; 
+    }
+}
