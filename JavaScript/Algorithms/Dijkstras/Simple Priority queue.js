07fccb19fab0a21aaa319e0a3fbb0b87047d06d8
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-14 12:00:26 +0200

    simple priority queue

---
+++ JavaScript/Algorithms/Dijkstras/Simple Priority queue.js
@@ -0,0 +1,17 @@
+// Notice we are sorting which is O(N * log(N))
+
+class PriorityQueue {
+    constructor(){
+      this.values = [];
+    }
+    enqueue(val, priority) {
+      this.values.push({val, priority});
+      this.sort();
+    };
+    dequeue() {
+      return this.values.shift();
+    };
+    sort() {
+      this.values.sort((a, b) => a.priority - b.priority);
+    };
+  }
