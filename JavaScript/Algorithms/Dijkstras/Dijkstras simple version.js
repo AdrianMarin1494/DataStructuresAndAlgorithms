c54babb7613f5ad0a73d118c68dc223612b91b85
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-14 14:13:08 +0200

    dijkstras

--- JavaScript/Algorithms/Dijkstras/Dijkstras simple version.js
+++ JavaScript/Algorithms/Dijkstras/Dijkstras simple version.js
@@ -1,12 +1,3 @@
-// Finds the shortest path between two vertices on a graph
-// "What's the fastest way to get from point A to point B?"
-// WHY IS IT USEFUL?
-// GPS - finding fastest route
-// Network Routing - finds open shortest path for data
-// Biology - used to model the spread of viruses among humans
-// Airline tickets - finding cheapest route to your destination
-// Many other uses!
-
 // Dijkstra's Pseudocode
 // This function should accept a starting and ending vertex
 // Create an object (we'll call it distances) and set each key to be every vertex in the adjacency list with a value of infinity, except for the starting vertex which should have a value of 0.
