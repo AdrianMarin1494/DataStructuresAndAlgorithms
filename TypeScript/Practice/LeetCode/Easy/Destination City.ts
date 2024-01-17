75f4ca04fabc68899536d4a6fc2a51ee344b8a9e
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-17 20:13:50 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Destination City.ts
+++ TypeScript/Practice/LeetCode/Easy/Destination City.ts
@@ -1,2 +1,19 @@
 // https://leetcode.com/problems/destination-city/description/
 
+function destCity(paths: string[][]): string {
+    if (paths.length === 1) {
+        return paths[0][1];
+    }
+
+    const startingDestinations = [];
+
+    for (let i = 0; i < paths.length; i++) {
+        startingDestinations.push(paths[i][0]);
+    }
+
+    for (let i = 0; i < paths.length; i++) {
+        if (!startingDestinations.includes(paths[i][1])) {
+            return paths[i][1];
+        }
+    }
+};
