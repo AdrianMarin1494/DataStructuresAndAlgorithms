76429396a94fc8bf74f400d774fa4dbab42bdfc5
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-17 20:27:18 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Destination City.cs
+++ C#/Practice/LeetCode/Easy/Destination City.cs
@@ -1,2 +1,22 @@
 // https://leetcode.com/problems/destination-city/description/
 
+public class Solution {
+    public string DestCity(IList<IList<string>> paths) {
+        if (paths.Count == 1) {
+            return paths[0][1];
+        }
+
+        List<string> startingDestinations = new List<string>();
+
+        for (int i = 0; i < paths.Count; i++) {
+            startingDestinations.Add(paths[i][0]);
+        }
+
+        for (int i = 0; i < paths.Count; i++) {
+            if (!startingDestinations.Contains(paths[i][1])) {
+                return paths[i][1];
+            }
+        }
+        return "";
+    }
+}
