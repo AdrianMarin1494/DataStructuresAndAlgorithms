82932f763f13420def3e3e6071ca19bc3196d1ea
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-17 20:10:30 +0200

    leetcode

--- JavaScript/Testing.js
+++ JavaScript/Testing.js
@@ -1,51 +1,24 @@
 /**
- * @param {number[][]} matches
- * @return {number[][]}
+ * @param {string[][]} paths
+ * @return {string}
  */
-var findWinners = function(matches) {
-    // object: key: player, value: loses
-    // {
-    //     "3": 2,
-    //     "6": 2,
-    //     "7": 1,
-    //     "5": 1,
-    //     "8": 1,
-    //     "9": 2,
-    //     "4": 1
-    // }
-    // take the players who lost only 1 from here
-    // if object[matches[i]] === undefined, the player didn t lost and add it to the first array
-
-    const playersOne = [];
-    const playersZero = [];
-    const playersLoses = {};
-
-    for (let i = 0; i < matches.length; i++) {
-        if (playersLoses[matches[i][1]] === undefined) {
-            playersLoses[matches[i][1]] = 1;
-        } else {
-            playersLoses[matches[i][1]] += 1;
-        }
+var destCity = function(paths) {
+    if (paths.length === 1) {
+        return paths[0][1];
     }
 
-    for (let player in playersLoses) {
-        if (playersLoses[player] === 1) {
-            playersOne.push(Number(player));
-        }
+    const startingDestinations = [];
+
+    for (let i = 0; i < paths.length; i++) {
+        startingDestinations.push(paths[i][0]);
     }
 
-    for (let i = 0; i < matches.length; i++) {
-        if (playersLoses[matches[i][0]] === undefined && !playersZero.includes(matches[i][0])) {
-            playersZero.push(matches[i][0]);
+    for (let i = 0; i < paths.length; i++) {
+        if (!startingDestinations.includes(paths[i][1])) {
+            return paths[i][1];
         }
     }
-
-    playersOne.sort((a, b) => a - b);
-    playersZero.sort((a, b) => a - b);
-
-    console.log([playersZero], [playersOne]);
-    return [[...playersZero], [...playersOne]];
 };
 
-
-findWinners([[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]);
+console.log(destCity([["B","C"],["D","B"],["C","A"]]))
+console.log(destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]))
