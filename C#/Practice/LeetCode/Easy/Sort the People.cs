9c4353c103fa9f6773438b7668d5908c5a14d209
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-17 21:04:04 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Sort the People.cs
+++ C#/Practice/LeetCode/Easy/Sort the People.cs
@@ -1,2 +1,22 @@
 // https://leetcode.com/problems/sort-the-people/
 
+public class Solution {
+    public string[] SortPeople(string[] names, int[] heights) {
+        List<Tuple<string, int>> namesAndHeights = new List<Tuple<string, int>>();
+        List<string> sortedPeople = new List<string>();
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            namesAndHeights.Add(Tuple.Create(names[i], heights[i]));
+        }
+
+        namesAndHeights = namesAndHeights.OrderByDescending(t => t.Item2).ToList();
+
+        for (int i = 0; i < namesAndHeights.Count; i++)
+        {
+            sortedPeople.Add(namesAndHeights[i].Item1);
+        }
+
+        return sortedPeople.ToArray();
+    }
+}
