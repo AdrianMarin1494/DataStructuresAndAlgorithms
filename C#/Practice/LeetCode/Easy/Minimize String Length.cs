7df5f3625426dd0abd9a281544d60f6c59878ad8
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-18 22:27:17 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Minimize String Length.cs
+++ C#/Practice/LeetCode/Easy/Minimize String Length.cs
@@ -1,2 +1,11 @@
 // https://leetcode.com/problems/minimize-string-length/description/
 
+public class Solution {
+    public int MinimizedStringLength(string s) {
+        HashSet<char> uniqueValues = new HashSet<char>(s);
+        for (int i = 0; i < s.Length; i++) {
+            uniqueValues.Add(s[i]);
+        }
+        return uniqueValues.Count;
+    }
+}
