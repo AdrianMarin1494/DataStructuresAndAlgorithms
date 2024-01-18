3d301eeffa4f00ff511a5ed3d950bb52b4a59504
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-18 21:54:13 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Robot Return to Origin.cs
+++ C#/Practice/LeetCode/Easy/Robot Return to Origin.cs
@@ -1,2 +1,26 @@
 // https://leetcode.com/problems/robot-return-to-origin/description/
 
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int vertical = 0;
+        int horizontal = 0;
+
+        for (int i = 0; i < moves.Length; i++) {
+            if (moves[i] == 'U') {
+                vertical++;
+            } else if (moves[i] == 'D') {
+                vertical--;
+            } else if (moves[i] == 'L') {
+                horizontal--;
+            } else if (moves[i] == 'R') {
+                horizontal++;
+            }
+        }
+
+        if (vertical == 0 && horizontal == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
