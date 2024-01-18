7a5f79ee4050637ff3cfad0c67839fa6ad932dd5
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-18 21:27:40 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Baseball Game.cs
+++ C#/Practice/LeetCode/Easy/Baseball Game.cs
@@ -1,2 +1,26 @@
 // https://leetcode.com/problems/baseball-game/description/
 
+public class Solution {
+    public int CalPoints(string[] operations) {
+        List<int> digits = new List<int>();
+        int score = 0;
+
+        for (int i = 0; i < operations.Length; i++) {
+            if (operations[i] == "+") {
+                digits.Add(digits[digits.Count-1] + digits[digits.Count-2]);
+            } else if (operations[i] == "D") {
+                digits.Add(digits[digits.Count-1] * 2);
+            } else if (operations[i] == "C") {
+                digits.RemoveAt(digits.Count-1);
+            } else {
+                digits.Add(int.Parse(operations[i]));
+            }
+        }
+
+        for (int i = 0; i < digits.Count; i++) {
+            score += digits[i];
+        }
+
+        return score;
+    }
+}
