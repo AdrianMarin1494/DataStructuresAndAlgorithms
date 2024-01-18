f4ce0d36bd89ce46437fd6658f66e7ce5b1eb8e0
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-18 21:15:09 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Baseball Game.ts
+++ TypeScript/Practice/LeetCode/Easy/Baseball Game.ts
@@ -1,2 +1,24 @@
 // https://leetcode.com/problems/baseball-game/description/
 
+function calPoints(operations: string[]): number {
+    const digits: number[] = [];
+    let score: number = 0;
+
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i] === "+") {
+            digits.push(digits[digits.length-1] + digits[digits.length-2]);
+        } else if (operations[i] === "D") {
+            digits.push(digits[digits.length-1] * 2);
+        } else if (operations[i] === "C") {
+            digits.pop();
+        } else {
+            digits.push(Number(operations[i]));
+        }
+    }
+
+    for (let i = 0; i < digits.length; i++) {
+        score += digits[i];
+    }
+
+    return score;   
+};
