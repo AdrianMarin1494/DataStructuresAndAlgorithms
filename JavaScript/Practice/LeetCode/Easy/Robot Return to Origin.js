5f5df3f5cf8df92d0966b41d0d9858d9b8c1aa3b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-18 21:49:02 +0200

    leetcode

--- JavaScript/Practice/LeetCode/Easy/Robot Return to Origin.js
+++ JavaScript/Practice/LeetCode/Easy/Robot Return to Origin.js
@@ -5,17 +5,40 @@
  * @return {boolean}
  */
 var judgeCircle = function(moves) {
-    const movesFrequency = {};
+    // Solution 1
+    // const movesFrequency = {};
+
+    // for (let i = 0; i < moves.length; i++) {
+    //     if (movesFrequency[moves[i]] === undefined) {
+    //         movesFrequency[moves[i]] = 1;
+    //     } else {
+    //         movesFrequency[moves[i]] += 1;
+    //     }
+    // }
+
+    // if (movesFrequency["U"] === movesFrequency["D"] && movesFrequency["L"] === movesFrequency["R"]) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    // Solution 2;
+    let vertical = 0;
+    let horizontal = 0;
 
     for (let i = 0; i < moves.length; i++) {
-        if (movesFrequency[moves[i]] === undefined) {
-            movesFrequency[moves[i]] = 1;
-        } else {
-            movesFrequency[moves[i]] += 1;
+        if (moves[i] === 'U') {
+            vertical++;
+        } else if (moves[i] === 'D') {
+            vertical--;
+        } else if (moves[i] === 'L') {
+            horizontal--;
+        } else if (moves[i] === 'R') {
+            horizontal++;
         }
     }
 
-    if (movesFrequency["U"] === movesFrequency["D"] && movesFrequency["L"] === movesFrequency["R"]) {
+    if (vertical === 0 && horizontal === 0) {
         return true;
     } else {
         return false;
