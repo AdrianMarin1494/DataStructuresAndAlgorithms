fc743b18397820feb48d9cbc29900d953d5b283c
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-18 21:50:37 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Robot Return to Origin.ts
+++ TypeScript/Practice/LeetCode/Easy/Robot Return to Origin.ts
@@ -1,2 +1,24 @@
 // https://leetcode.com/problems/robot-return-to-origin/description/
 
+function judgeCircle(moves: string): boolean {
+    let vertical: number = 0;
+    let horizontal: number = 0;
+
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i] === 'U') {
+            vertical++;
+        } else if (moves[i] === 'D') {
+            vertical--;
+        } else if (moves[i] === 'L') {
+            horizontal--;
+        } else if (moves[i] === 'R') {
+            horizontal++;
+        }
+    }
+
+    if (vertical === 0 && horizontal === 0) {
+        return true;
+    } else {
+        return false;
+    }    
+};
