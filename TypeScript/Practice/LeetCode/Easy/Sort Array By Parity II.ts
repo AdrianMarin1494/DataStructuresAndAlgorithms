e10c4ff9e165e233f6715e3ba2deb97c98d9698c
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-19 21:10:30 +0200

    leetcode

--- TypeScript/Practice/LeetCode/Easy/Sort Array By Parity II.ts
+++ TypeScript/Practice/LeetCode/Easy/Sort Array By Parity II.ts
@@ -1,2 +1,22 @@
 // https://leetcode.com/problems/sort-array-by-parity-ii/description/
 
+function sortArrayByParityII(nums: number[]): number[] {
+    const odds: number[] = [];
+    const evens: number[] = [];
+    const result: number[] = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            evens.push(nums[i]);
+        } else {
+            odds.push(nums[i]);
+        }
+    }
+
+    for (let i = 0; i < evens.length; i++) {
+        result.push(evens[i]);
+        result.push(odds[i]);
+    }
+
+    return result; 
+};
