d35788fdff6d27b6633a85615498eda978f791a4
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-19 21:20:52 +0200

    leetcode

--- C#/Practice/LeetCode/Easy/Sort Array By Parity II.cs
+++ C#/Practice/LeetCode/Easy/Sort Array By Parity II.cs
@@ -1,2 +1,24 @@
 // https://leetcode.com/problems/sort-array-by-parity-ii/description/
 
+public class Solution {
+    public int[] SortArrayByParityII(int[] nums) {
+        List<int> odds = new List<int>();
+        List<int> evens = new List<int>();
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] % 2 == 0) {
+                evens.Add(nums[i]);
+            } else {
+                odds.Add(nums[i]);
+            }
+        }
+
+        for (int i = 0; i < evens.Count; i++) {
+            result.Add(evens[i]);
+            result.Add(odds[i]);
+        }
+
+        return result.ToArray();  
+    }
+}
