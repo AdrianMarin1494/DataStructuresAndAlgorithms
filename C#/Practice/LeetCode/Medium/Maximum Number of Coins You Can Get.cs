875494ff41ddfa09d8f412227afa61b885f8c702
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-22 21:13:44 +0200

    leetcode

--- C#/Practice/LeetCode/Medium/Maximum Number of Coins You Can Get.cs
+++ C#/Practice/LeetCode/Medium/Maximum Number of Coins You Can Get.cs
@@ -1,2 +1,20 @@
 // https://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
 
+public class Solution {
+    public int MaxCoins(int[] piles) {
+        int sum = 0;
+        int counter = 0;
+        int idx = 1;
+
+        Array.Sort(piles, (a, b) => b - a);
+
+        while (counter < piles.Length / 3)
+        {
+            sum += piles[idx];
+            idx += 2;
+            counter++;
+        }
+
+        return sum;
+    }
+}
