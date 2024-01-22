d598c76be2980f856e7d03613c186164cebfff8b
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-22 21:20:23 +0200

    leetcode

--- Java/Practice/LeetCode/Medium/Maximum Number of Coins You Can Get.java
+++ Java/Practice/LeetCode/Medium/Maximum Number of Coins You Can Get.java
@@ -1,2 +1,19 @@
 // https://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
 
+class Solution {
+    public int maxCoins(int[] piles) {
+        int sum = 0;
+        int counter = 0;
+        int idx = piles.length - 2;
+
+        Arrays.sort(piles);
+
+        while (counter < piles.length / 3) {
+            sum += piles[idx];
+            idx -= 2;
+            counter++;
+        }
+
+        return sum;
+    }
+}
