6905c6f005ce0dd568c5628e99d0ed3c289f1c9f
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-22 21:12:30 +0200

    leetcode

--- Python/Practice/LeetCode/Medium/Maximum Number of Coins You Can Get.py
+++ Python/Practice/LeetCode/Medium/Maximum Number of Coins You Can Get.py
@@ -1,2 +1,16 @@
 # https://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
 
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        sum_val = 0
+        counter = 0
+        idx = 1
+
+        piles.sort(reverse=True)
+
+        while counter < len(piles) / 3:
+            sum_val += piles[idx]
+            idx += 2
+            counter += 1
+
+        return sum_val
