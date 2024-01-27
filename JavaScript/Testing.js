edc53dc202c673bc0f8c9e3dd9f5130500d2d0b0
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-27 12:41:15 +0200

    leetcode

--- JavaScript/Testing.js
+++ JavaScript/Testing.js
@@ -63,4 +63,25 @@ class BinarySearchTree {
 
         return current;
     }
+
+    contains(value) {
+        if (this.root === null) {
+            return false;
+        }
+
+        let current = this.root;
+        let found = false;
+
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
