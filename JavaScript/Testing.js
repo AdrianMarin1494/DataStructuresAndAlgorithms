75255cffa501937d946f5c2bf938c7d2d16916e1
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-01-28 14:32:44 +0200

    leetcode

--- JavaScript/Testing.js
+++ JavaScript/Testing.js
@@ -102,4 +102,22 @@ class BinarySearchTree {
 
         return data;
     }
+
+    DFSPostOrder() {
+        const data = [];
+
+        function traverse(node) {
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        
+        return data;
+    }
 }
