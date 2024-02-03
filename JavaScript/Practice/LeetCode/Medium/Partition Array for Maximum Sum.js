936e0bf5f36a14aa0981ed88fb124f2b354e9285
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-02-03 19:31:31 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Medium/Partition Array for Maximum Sum.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/partition-array-for-maximum-sum/description/?envType=daily-question&envId=2024-02-03
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumAfterPartitioning = function(arr, k) {
+    let n = arr.length;
+    let dp = new Array(n + 1).fill(0);
+    
+    for (let i = 1; i <= n; i++) {
+        let max = 0;
+        let maxSum = 0;
+        for (let j = 1; j <= k && i - j >= 0; j++) {
+            max = Math.max(max, arr[i - j]);
+            maxSum = Math.max(maxSum, dp[i - j] + max * j);
+        }
+        dp[i] = maxSum;
+    }
+    
+    return dp[n];
+};
