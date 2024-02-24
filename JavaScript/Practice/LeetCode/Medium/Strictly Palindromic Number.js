15106c232355fa8103230a96a6e90ab9e0e8a7d0
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-02-24 18:45:17 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Medium/Strictly Palindromic Number.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/strictly-palindromic-number/description/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isStrictlyPalindromic = function(n) {
+    return false;
+};
