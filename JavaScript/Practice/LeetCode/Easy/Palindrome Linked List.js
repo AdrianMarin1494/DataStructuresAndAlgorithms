793cdef2e5783eb0c9ea3e990a02c192583a9fb9
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-03-22 21:51:01 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Easy/Palindrome Linked List.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/palindrome-linked-list/description/?envType=daily-question&envId=2024-03-22
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    const listVals = [];
+    while (head) {
+        listVals.push(head.val);
+        head = head.next;
+    }
+    
+    let left = 0, right = listVals.length - 1;
+    while (left < right && listVals[left] === listVals[right]) {
+        left++;
+        right--;
+    }
+    return left >= right;
+};
