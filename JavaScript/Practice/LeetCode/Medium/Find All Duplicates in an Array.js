a0e6b79b52ae448bcf610fde28cb39e2decfc284
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-03-25 19:10:37 +0200

    leetcode

---
+++ JavaScript/Practice/LeetCode/Medium/Find All Duplicates in an Array.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/description/?envType=daily-question&envId=2024-03-25
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    const result = [];
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const num = Math.abs(nums[i]);
+        const idx = num - 1;
+        if (nums[idx] < 0)
+            result.push(num);
+        nums[idx] *= -1;
+    }
+    return result;
+};
