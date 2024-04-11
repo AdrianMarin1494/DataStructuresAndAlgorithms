c09f1e8a8c61c7cea1fe403f0b4e97f68ea40887
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-04-11 14:34:12 +0300

    leetcode

---
+++ JavaScript/Practice/LeetCode/Medium/Remove K Digits.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/remove-k-digits/description/?envType=daily-question&envId=2024-04-11
+
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    if(num.length ==k) return '0'
+    const res = []
+   for(let i=0;i<num.length;i++){
+        while(k>0 && res.length>0 && res[res.length-1]>num[i]){
+            res.pop()
+            k--
+        }
+        
+        if(res.length ==0 && num[i]=='0') continue
+        res.push(num[i])
+   }
+
+   while(k>0 && res.length>0) {
+    k--;
+    res.pop();
+   }
+
+   return res.length ==0 ?'0':res.join('')
+};
