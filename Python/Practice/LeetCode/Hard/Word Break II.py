2730abc8b8b6c5e66161756fd9084f70bee950da
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-05-25 09:37:12 +0300

    leetcode

---
+++ Python/Practice/LeetCode/Hard/Word Break II.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/word-break-ii/description/?envType=daily-question&envId=2024-05-25
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+      
+        ans = []
+        wordSet = set(wordDict)  # Convert list to set for efficient lookup
+
+        def dfs(pref: str, suff: str) -> None:
+            if suff == '':
+                ans.append(pref.rstrip())  # Add the valid sentence to the answer list
+            else:
+                for i in range(len(suff)):
+                    if suff[:i+1] not in wordSet: 
+                        continue
+                    dfs(pref + suff[:i+1] + ' ', suff[i+1:])  # Fix slicing issue
+
+        dfs('', s)
+        return ans
