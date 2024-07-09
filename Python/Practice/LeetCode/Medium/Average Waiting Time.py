36a51405f55b26191f9e41e6233e4e233d17a56f
AdrianMarin1494 <marin.adrian1494@gmail.com>
2024-07-09 19:45:03 +0300

    leetcode

---
+++ Python/Practice/LeetCode/Medium/Average Waiting Time.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/average-waiting-time/description/?envType=daily-question&envId=2024-07-09
+
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        available_at = 0
+        total_wait = 0
+        for arrival, t in customers:
+            available_at = max(available_at, arrival) + t
+            total_wait += available_at - arrival
+        
+        return total_wait / len(customers)
